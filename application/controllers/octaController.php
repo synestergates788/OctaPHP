1a9abd62168cf942301e50f767444eabddf40496
milky <afacebu.milky@gmail.com>
2019-07-08 15:45:53 +0800

    testing travix_ci integration

--- application/controllers/octaController.php
+++ application/controllers/octaController.php
@@ -3,6 +3,6 @@
 class octaController extends Controller{
 
     public function index(){
-        $this->view('index.html.twig');
+        $this->view('index.html.twig'); #test travis_ci integration
     }
 }
